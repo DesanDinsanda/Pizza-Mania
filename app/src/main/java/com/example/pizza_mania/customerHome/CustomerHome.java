2205618c240de1e85795b565c49624e9c95ff987
H1runa <hirunadilmith12@gmail.com>
2025-09-12 23:44:22 +0530

    Auto selects the branch with MyLocation

--- app/src/main/java/com/example/pizza_mania/customerHome/CustomerHome.java
+++ app/src/main/java/com/example/pizza_mania/customerHome/CustomerHome.java
@@ -1,7 +1,13 @@
 package com.example.pizza_mania.customerHome;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -9,14 +15,24 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.pizza_mania.GlobalApp;
 import com.example.pizza_mania.R;
 import com.example.pizza_mania.customerAccount.AccountSettings;
 import com.example.pizza_mania.utils.BottomNavigationHelper;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +51,14 @@ public class CustomerHome extends AppCompatActivity {
     FirebaseFirestore db;
     FirebaseUser currentUser;
     String uid;
+    private View overlayLoading;
+    private TextView fetching_location_text;
+    private Animation text_loading_animation;
+    private int locationPermReqCode = 01;
+    private LatLng currentLocation;
+
+    private FusedLocationProviderClient flpClient;
+    private GlobalApp globalApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +70,17 @@ public class CustomerHome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //adding fetch location loading anim
+        globalApp = (GlobalApp) getApplication();
+        ViewGroup rootView = findViewById(android.R.id.content);
+        overlayLoading = getLayoutInflater().inflate(R.layout.loading_fetching_location, rootView, false);
+        rootView.addView(overlayLoading);
+        fetching_location_text = overlayLoading.findViewById(R.id.fetchingLocationText);
+        text_loading_animation = AnimationUtils.loadAnimation(this, R.anim.loading_text_anim);
+
+        flpClient = LocationServices.getFusedLocationProviderClient(this); //for fetching current location
+
 
         txtFirstName = findViewById(R.id.txtFirstName);
         loadingLayout = findViewById(R.id.loadingLayout);
@@ -59,6 +94,8 @@ public class CustomerHome extends AppCompatActivity {
         loadCustomerDetails();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         BottomNavigationHelper.setupBottomNavigation(this, bottomNavigationView, R.id.nav_home);
+
+        fetchLocation();
     }
 
     public void loadCustomerDetails(){
@@ -89,5 +126,39 @@ public class CustomerHome extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void fetchLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},locationPermReqCode );
+        }
+        showLoadingAnim();
+        CurrentLocationRequest req = new CurrentLocationRequest.Builder().setPriority(Priority.PRIORITY_HIGH_ACCURACY).build();
+        flpClient.getCurrentLocation(req, null).addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                hideLoadingAnim();
+                currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                globalApp.setCurrentLocation(currentLocation);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                fetching_location_text.clearAnimation();
+                fetchLocation();
+            }
+        });
+    }
+
+    private void showLoadingAnim(){
+        fetching_location_text.startAnimation(text_loading_animation);
+        overlayLoading.setVisibility(View.VISIBLE);
+        overlayLoading.setClickable(true);
+    }
+    private void hideLoadingAnim(){
+        fetching_location_text.clearAnimation();
+        overlayLoading.setClickable(false);
+        overlayLoading.setVisibility(View.GONE);
     }
 }
