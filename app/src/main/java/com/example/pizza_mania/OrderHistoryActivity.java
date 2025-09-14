aa885e0ba5da76c5823d1eca86d35cacfac8fa03
NethmiVP <nethmi.punsala@gmail.com>
2025-09-14 20:07:19 +0530

    Updated the Java file and the XML file of Order History

--- app/src/main/java/com/example/pizza_mania/OrderHistoryActivity.java
+++ app/src/main/java/com/example/pizza_mania/OrderHistoryActivity.java
@@ -1,6 +1,9 @@
 package com.example.pizza_mania;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class OrderHistoryActivity extends AppCompatActivity {
 
+    Button btOngoing;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_order_history);
+
+        // Fix window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Initialize button
+        btOngoing = findViewById(R.id.btOngoing);
+
+        // Set button click listener
+        btOngoing.setOnClickListener(v -> {
+            Intent intent = new Intent(OrderHistoryActivity.this, OrderStatusActivity.class);
+            startActivity(intent);
         });
     }
 }
